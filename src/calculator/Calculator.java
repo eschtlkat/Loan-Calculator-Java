281703a7ab0e7db2d59f53b6f91f31f302cdeb27
Laura Ukrinaitė <laura.ukrinaite@mif.stud.vu.lt>
2024-03-20 20:37:45 +0200

    added new package

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,18 @@
+package calculator;
+
+public class Calculator{
+	int whichGraph, month, year; 
+	double loanSum, percentage;
+	
+	public Calculator(int whichGraph, int month, int year, double loanSum, double percentage) {
+		this.whichGraph = whichGraph;
+		this.month = month;
+		this.year = year;
+		this.loanSum = loanSum;
+		this.percentage = percentage;
+		
+		
+		
+	}
+
+}
